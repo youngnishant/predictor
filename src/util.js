60c56e73250f01e58dac4f2f0a16ae21355a8f5f
Nishant <youngnishant@outlook.com>
2022-02-17 22:12:18 +0530

    finally working

--- src/util.js
+++ src/util.js
@@ -1,6 +1,10 @@
 import Papa from "papaparse";
-const { chunk } = require("lodash");
-const brain = require("brain.js");
+import { chunk } from "lodash";
+import * as brain from "brain.js";
+
+let max = 0;
+let min = 1000000;
+
 const parseData = async (file) => {
   let data = [];
   if (file) {
@@ -9,44 +13,55 @@ const parseData = async (file) => {
         results.data.forEach((item) => {
           data.push(item.slice(2, 6));
         });
+        data.shift();
         let rawTrainingData = [];
-        data.map((item) => rawTrainingData.push(scaleDown(item)));
-        rawTrainingData.shift();
+        data.map((item) => {
+          if (convertToFloat(item[1]) > max) max = convertToFloat(item[1]);
+          if (convertToFloat(item[2]) < min) min = convertToFloat(item[2]);
+        });
+        data.map((item) => {
+          rawTrainingData.push(scaleDown(item));
+        });
         let temp = chunk(rawTrainingData, 5);
-        let trainingData = temp.slice(0, 3);
-        console.log(trainingData);
+        let trainingData = temp.slice(0, 11);
+        console.log(rawTrainingData, max, min);
         const net = new brain.recurrent.LSTMTimeStep({
           inputSize: 4,
           hiddenLayers: [8, 8],
-          outputSize: 4
+          outputSize: 4,
         });
         net.train(trainingData, {
           learningRate: 0.05,
           errorThresh: 0.02,
-          log: true
+          log: true,
         });
-        let forecastData = temp[3];
-        let originalData = temp[3][0];
+        let forecastData = temp[11];
+        let originalData = temp[12][0];
         console.log(
           scaleUp(net.forecast(forecastData, 1)[0]),
           scaleUp(originalData)
         );
-      }
+      },
     });
   }
 };
 
 const scaleDown = (item) => ({
-  open: parseFloat(item[0].replace(",", "")) / 2410,
-  high: parseFloat(item[1].replace(",", "")) / 2410,
-  low: parseFloat(item[2].replace(",", "")) / 2410,
-  close: parseFloat(item[3].replace(",", "")) / 2410
+  open: normalise(convertToFloat(item[0])),
+  high: normalise(convertToFloat(item[1])),
+  low: normalise(convertToFloat(item[2])),
+  close: normalise(convertToFloat(item[3])),
 });
 
 const scaleUp = (item) => ({
-  open: item.open * 2410,
-  high: item.high * 2410,
-  low: item.low * 2410,
-  close: item.close * 2410
+  open: denormalise(item.open),
+  high: denormalise(item.high),
+  low: denormalise(item.low),
+  close: denormalise(item.close),
 });
+
+const convertToFloat = (value) => parseFloat(value.replace(",", ""));
+const normalise = (value) => (value - min) / (max - min);
+const denormalise = (value) => value * (max - min) + min;
+
 export default parseData;
