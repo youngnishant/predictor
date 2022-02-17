df5cc854312309dc4cf094802b366a5c514eeed7
Nishant Sharma <47674286+youngnishant@users.noreply.github.com>
2022-02-17 16:59:22 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,17 @@
 import parseData from "./util";
-
+import { useState } from "react";
 export default function App() {
+  const [file, setFile] = useState(null);
   return (
-    <div className="App">
-      <input type="file" accept=".csv,.xlsx,.xls" onChange={parseData} />
-    </div>
+    <>
+      <div className="App">
+        <input
+          type="file"
+          accept=".csv,.xlsx,.xls"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+      </div>
+      <button onClick={() => parseData(file)}>Check</button>
+    </>
   );
 }
